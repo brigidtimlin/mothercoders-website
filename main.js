09769d4fb7c2bef8b37daf724eb9e9fad6086f3d
brigidtimlin <brigidtimlin@gmail.com>
2019-04-06 21:50:02 -0400

    added js, updated css & images

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log ("this is brigid");
+function hover(element) {
+    element.setAttribute('src', 'img/fun-photo.jpg');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'img/website-profile-pic.jpg');
+  }
