10115d662a4ef907ec8b3a0196e72d52a0895a19
brigidtimlin <brigidtimlin@gmail.com>
2019-07-28 12:05:27 -0400

    created Docs folder

---
+++ Docs/main.js
@@ -0,0 +1,8 @@
+console.log ("this is brigid");
+function hover(element) {
+    element.setAttribute('src', 'img/familyMuseum.jpg');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', 'img/website-profile-pic.jpg');
+  }
